d95cc5db686efbf8d072b5af1e0d569d8e878e8a
moinshaikh195 <shaikhmoin.abdul195@gmail.com>
2021-05-22 12:26:41 +0530

    first commit

---
+++ src/Cards.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+function Cards(){
+    return(
+      <React.Fragment>
+       {/* <div className="card-deck">
+        <div className="card"  >
+  <img className="card-img-top" src="..." alt="Card image cap"/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div className="card" >
+  <img className="card-img-top" src="..." alt="Card image cap"/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div className="card" >
+  <img className="card-img-top" src="..." alt="Card image cap"/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div> */}
+
+<CardDeck>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+</CardDeck>
+
+        </React.Fragment>
+    );
+}
+export default Cards;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Cards from './Cards';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Cards />
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
